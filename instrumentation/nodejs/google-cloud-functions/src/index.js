ab6332591b3c2e211e55184cd31396466efcec4c
dmitchsplunk <derekmitchell@splunk.com>
2025-03-03 14:14:50 -0800

    published a node.js google cloud function example

--- instrumentation/nodejs/google-cloud-functions/src/index.js
+++ instrumentation/nodejs/google-cloud-functions/src/index.js
@@ -1,25 +1,6 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
-const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-const { BatchLogRecordProcessor } = require('@opentelemetry/sdk-logs');
-const { OTLPLogExporter } = require ('@opentelemetry/exporter-logs-otlp-http');
 const functions = require('@google-cloud/functions-framework');
 const logger = require('pino')()
 const opentelemetry = require('@opentelemetry/api');
-
-const sdk = new NodeSDK({
-    traceExporter: new OTLPTraceExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new OTLPMetricExporter(),
-    }),
-    logRecordProcessor: new BatchLogRecordProcessor(
-        new OTLPLogExporter()
-    ),
-    instrumentations: [getNodeAutoInstrumentations()],
-});
-sdk.start();
 
 const tracer = opentelemetry.trace.getTracer('google-cloud-function-nodejs-opentelemetry-example', '0.1.0');
 
