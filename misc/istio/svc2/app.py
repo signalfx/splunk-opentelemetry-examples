0cb71d1f152d64cfb7daa61447ca79379695aa21
dmitchsplunk <derekmitchell@splunk.com>
2025-04-10 17:22:02 -0700

    updated istio example

--- misc/istio/svc2/app.py
+++ misc/istio/svc2/app.py
@@ -7,5 +7,7 @@ logging.getLogger().setLevel(logging.INFO)
 @app.route("/hello")
 def hello_world():
 
+    print(request.headers)
+
     logging.getLogger().info("Handling the /hello request")
     return "Hello from Service 2!"
