b9a518dc34ba2b1b13168d9e14ccc95f345220e5
dmitchsplunk <derekmitchell@splunk.com>
2025-08-14 14:08:48 -0700

    added handoff to openai agents sdk example

--- gen-ai/openai-agents/math_problems/src/student_agent.py
+++ gen-ai/openai-agents/math_problems/src/student_agent.py
@@ -1,15 +1,20 @@
 from agents import Agent
+from teaching_assistant_agent import teaching_assistant_agent
 
 INSTRUCTIONS = (
     "You're a meticulous Grade 8 student with a keen eye for detail. You're known for "
     "your ability to apply logic to just about any math problem the teacher "
     "gives you.  Yet sometimes, you make things too darn complicated.\n"
     "Your task is to review the assigned math question and prepare a solution.\n"
-    "The answer should be in markdown format and should show the steps you used to find the solution."
+    "The answer should be in markdown format and should show the steps you used to find the solution.\n"
+    "Pass the final solution to the 'Teaching Assistant' agent."
 )
+
+handoffs = [teaching_assistant_agent]
 
 student_agent = Agent(
     name="StudentAgent",
     instructions=INSTRUCTIONS,
+    handoffs=handoffs,
     model="gpt-4o-mini"
 )
