3bda29839d1d214344a0e32983a0a924d45e7037
dmitchsplunk <derekmitchell@splunk.com>
2025-09-22 20:08:44 -0700

    added logging to azure app service example

--- instrumentation/java/azure-app-service/src/main/java/com/example/restservice/greeting/GreetingController.java
+++ instrumentation/java/azure-app-service/src/main/java/com/example/restservice/greeting/GreetingController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @RestController
 public class GreetingController {
+
+    private static final Logger logger = LogManager.getLogger(GreetingController.class);
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
@@ -16,6 +21,7 @@ public class GreetingController {
     @CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	    logger.info("executing the /greeting request");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
