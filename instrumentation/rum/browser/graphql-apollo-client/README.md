394620257fa11e2a82a2e76e817dcd09dbf39126
dmitchsplunk <derekmitchell@splunk.com>
2025-11-28 15:06:50 -0800

    added graphql apollo client RUM example

---
+++ instrumentation/rum/browser/graphql-apollo-client/README.md
@@ -0,0 +1,74 @@
+# apollo-client-example
+
+This example is based off of https://www.apollographql.com/docs/react/get-started. 
+
+## Prerequisites
+
+A recent version of Node.js and NPM are required. These specific versions were used for testing: 
+
+* node.js v24.11.0
+* npm v11.6.1
+
+## Install Dependencies
+
+``` bash
+npm install @apollo/client graphql rxjs
+npm install -D vite @vitejs/plugin-react
+npm install react react-dom 
+npm install @opentelemetry/api
+```
+
+## Add RUM MetricSets
+
+In Splunk Observability Cloud, add new RUM MetricSets for the following tags: 
+
+* `graphql.operation.name`
+* `graphql.operation.type`
+* `graphql.endpoint`
+
+![RUM MetricSets](images/rum-metric-set.png)
+
+This ensures that GraphQL-related tags are available with features such as Tag Spotlight. 
+
+## Configure RUM Settings
+
+Edit the [index.html](./index.html) page and add your realm, RUM access token, 
+application name, and environment name: 
+
+````
+        SplunkRum.init({
+            realm: "your_realm",
+            rumAccessToken: "your_access_token",
+            applicationName: "your_app_name",
+            deploymentEnvironment: "your_environment_name",
+        });
+````
+
+## Run the Application 
+
+``` bash
+npm run dev
+```
+
+Access the application by pointing your browser to `http://localhost:5173/`.  
+
+## Custom Events
+
+After generating some traffic, navigate to the Custom Events tab for this RUM application 
+in Splunk Observability Cloud. Here we can see that a custom event has been captured for the GraphQL 
+operation that's invoked by Apollo Client, which in this case is `GetLocations`. 
+
+![RUM Custom Events](images/custom-events.png)
+
+## Tag Spotlight 
+
+In Splunk Observability Cloud, navigate to the Tag Spotlight page for 
+your RUM application.  See [Analyze service performance with Tag Spotlight](https://help.splunk.com/en/splunk-observability-cloud/monitor-end-user-experience/real-user-monitoring/analyze-services-with-span-tags/analyze-service-performance-with-tag-spotlight) 
+for details. 
+
+With Tag Spotlight, we can analyze performance of our web application in the context of
+values for the `graphql.operation.name`, `graphql.operation.type`, and `graphql.endpoint` tag values.
+
+![RUM Tag Spotlight](images/tag-spotlight.png)
+ 
+
---
+++ instrumentation/rum/browser/graphql-apollo-client/main.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloLink,
+    HttpLink,
+} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import { Observable } from "@apollo/client/utilities";
+import { trace } from "@opentelemetry/api";
+import App from "./App";
+
+const GRAPHQL_ENDPOINT = "https://flyby-router-demo.herokuapp.com/";
+
+// HttpLink to your GraphQL endpoint.
+const httpLink = new HttpLink({
+    uri: GRAPHQL_ENDPOINT,
+});
+
+// OpenTelemetry tracer for GraphQL spans.
+const tracer = trace.getTracer("frontend-graphql");
+
+// Apollo Link that creates a span for every GraphQL operation.
+const tracingLink = new ApolloLink((operation, forward) => {
+    if (!forward) {
+        // If there is no next link, just return the observable from forward.
+        return null;
+    }
+
+    const { operationName, query, variables } = operation;
+
+    // Derive operation type: query / mutation / subscription.
+    const mainDef = query.definitions?.find(
+        (def) => def.kind === "OperationDefinition"
+    );
+    const opType = mainDef?.operation || "query";
+
+    const spanName = `graphql.${operationName || "anonymous"}`;
+
+    const span = tracer.startSpan(spanName, {
+        attributes: {
+            "workflow.name": operationName || "anonymous",
+            "graphql.operation.name": operationName || "anonymous",
+            "graphql.operation.type": opType,
+            "graphql.endpoint": GRAPHQL_ENDPOINT,
+            "graphql.variables.present":
+                !!variables && Object.keys(variables).length > 0,
+        },
+    });
+
+    if (typeof window !== "undefined") {
+        span.setAttribute("page.url", window.location.href);
+    }
+
+    // Wrap the next link's observable in a new Observable.
+    return new Observable((observer) => {
+        const subscription = forward(operation).subscribe({
+            next: (result) => {
+                if (result.errors && result.errors.length > 0) {
+                    span.setAttribute("graphql.status", "error");
+                    span.setAttribute("error", true);
+                    span.setAttribute(
+                        "error.message",
+                        result.errors.map((e) => e.message).join("; ")
+                    );
+                } else {
+                    span.setAttribute("graphql.status", "success");
+                }
+
+                // Example: Generate a custom RUM event for a specific operation.
+                if (operationName === "GetLocations" && window.SplunkRum?.addEvent) {
+                    window.SplunkRum.addEvent("graphql_GetLocations_completed", {
+                        graphql_operation_name: operationName || "anonymous",
+                        graphql_operation_type: opType,
+                        status:
+                            result.errors && result.errors.length > 0 ? "error" : "success",
+                    });
+                }
+
+                observer.next(result);
+            },
+            error: (networkError) => {
+                span.setAttribute("graphql.status", "network_error");
+                span.setAttribute("error", true);
+                span.setAttribute("error.message", networkError.message);
+                span.end();
+
+                observer.error(networkError);
+            },
+            complete: () => {
+                // End the span if it hasn't already been ended in `error`.
+                span.end();
+                observer.complete();
+            },
+        });
+
+        // Cleanup/unsubscribe.
+        return () => {
+            subscription.unsubscribe();
+        };
+    });
+});
+
+// Combine tracing link with the HTTP link.
+const link = ApolloLink.from([tracingLink, httpLink]);
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+});
+
+// Supported in React 18+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
